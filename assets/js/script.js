453384ae230f7358191fea5d64116d99a3a92e85
sammyniyo <samshakul@gmail.com>
2024-03-04 17:19:02 +0200

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,26 +113,27 @@ srRight.reveal('.form-control',{delay: 100})
 /* -- Contact Us-- */
 function sendMail() {
     var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
     };
-
-    const serviceID = "service_ax4h3gy"; 
-    const templateID = "template_uio2pm6"; 
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message sent successfully!!");
-        })
-        .catch(err => console.log(err));
-}
-
-
+  
+    const serviceID = "service_ax4h3gy";
+    const templateID = "template_uio2pm6";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
